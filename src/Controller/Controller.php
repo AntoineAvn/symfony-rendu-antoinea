0e81887dc803ef1476bd0f011d9771029e2ae188
Antoine A <antoine_avenia@hotmail.fr>
2022-11-17 16:59:15 +0100

    Get articles from dbb + show them on template

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class Controller extends AbstractController
 {
+
+    public function __construct(protected ManagerRegistry $registery){
+
+    }
+    
     #[Route('/', name: 'app_')]
     public function index(): Response
     {
-        return $this->render('/index.html.twig', [
+
+        $articleRegistery = $this->registery->getRepository(Article::class);
+        $articles = $articleRegistery->findAll();
+        
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No product found'
+            );
+        }
+
+        return $this->render('/Home/index.html.twig', [
             'controller_name' => 'Controller',
+            'articles' => $articles
         ]);
     }
 }

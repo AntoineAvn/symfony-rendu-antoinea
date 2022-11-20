0a8dfb663e0f3fddeeff06bc81c391eea29e21cf
Antoine A <antoine_avenia@hotmail.fr>
2022-11-20 17:02:55 +0100

    Add Ckeditor for WYSIWYG article content

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -15,7 +16,8 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
-            ->add('content')
+            // ->add('content')
+            ->add('content', CKEditorType::class)
             ->add('image')
             ->add('statut', CheckboxType::class, [
                 'label'    => 'Pour publier cet article cochez la case (Sinon il sera enregistré dans vos brouillons) ?',
